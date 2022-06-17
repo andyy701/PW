20fd4955bf6f865100b4895a13d884f9fb0b68f0
L34403 <L34403@lab.aca>
2022-06-17 13:08:36 -0500

    s111-backend

---
+++ s111-backend/index.js
@@ -0,0 +1,17 @@
+const express =require("express");
+const app = express();
+const PORT=3000;
+
+app.use(express.json());
+
+app.get("/tasks",(req,res) =>{
+       const task1={id}
+   }
+
+)
+
+app.listen(
+    PORT,_ =>{
+        console.log(`Escuchando en el puerto ${PORT}`);
+    }
+);
